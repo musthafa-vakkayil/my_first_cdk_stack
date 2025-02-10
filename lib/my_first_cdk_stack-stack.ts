a8b7afcdeae494692761c1ff75ea0e2a352da5c5
Muhammed Musthafa Shahal V <t-m.shahal.v@heart.org>
2025-02-10 12:46:24 +0530

    fix:cdk stack name

--- lib/my_first_cdk_stack-stack.ts
+++ lib/my_first_cdk_stack-stack.ts
@@ -3,7 +3,7 @@ import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 
-export class CdkStack extends cdk.Stack {
+export class MyFirstCdkStackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
